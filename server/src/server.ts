cb7a89bfefae952483068386aa7c5c9aa2c23254
田洪飞 <278948240@qq.com>
2022-09-07 10:02:47 +0800

    update .gitignore

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,12 @@ import router from './routers/routers'
 import koaStatic from 'koa-static'
 import {staticRoot} from './config/app'
 
+// 服务器添加跨域头 
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*')
+  await next()
+})
+
 app.use(router)
 app.use(koaStatic(staticRoot))
 
